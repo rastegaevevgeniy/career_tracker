d2146974cb078d2d72c776a0511ba3951646bbea
Iren4ik <124435764+Iren4ik@users.noreply.github.com>
2023-12-07 20:50:13 +0300

    FEAT/сверстать панель с прогрессом

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import "./Main.scss";
 import { useLocation } from "react-router-dom";
 import StartPage from "../StartPage/StartPage";
-// import TrackerBlock from "../TrackerBlock/TrackerBlock";
+import TrackerPage from "../TrackerPage/TrackerPage";
 
 function Main() {
   const { pathname } = useLocation();
 
   const startPage = pathname === "/";
-  // const trackerPage = pathname === "/recommendations";
+  const trackerPage = pathname === "/recommendations";
 
   return (
     <main className="main">
       {startPage && <StartPage />}
-      {/* {trackerPage && <TrackerBlock />} */}
+      {trackerPage && <TrackerPage />}
     </main>
   );
 }
---
+++ frontend/src/components/TrackerPage/TrackerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./TrackerPage.scss";
+import ProgressBlock from "../ProgressBlock/ProgressBlock";
+
+const TrackerPage: React.FC = () => {
+  
+  return (
+    <div className='trackerPage'>
+      <ProgressBlock />
+    </div>
+  );
+}
+
+export default TrackerPage;
