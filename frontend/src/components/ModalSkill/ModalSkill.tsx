147ba519b5f7026fe3085fa941093309c3cdf4b7
ValentinaZam <abc99-33@yandex.ru>
2023-12-11 00:36:15 +0300

    1

--- frontend/src/components/ModalSkill/ModalSkill.tsx
+++ frontend/src/components/ModalSkill/ModalSkill.tsx
@@ -29,7 +29,8 @@ const ModalSkill: React.FC = () => {
   useEffect(() => {
     api
       .getInitialTracker()
-      .then((data) => setDataUser(data))
+      // .then((data) => setDataUser(data))
+      .then((data) => console.log(data))
       .catch((err) => console.log(`Ошибка ${err}`))
   }, [])
 
@@ -103,37 +104,37 @@ const ModalSkill: React.FC = () => {
                 </Typography>
               </div>
 
-              {/* <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                 <div>
                   {dataUser && dataUser.map((item, index) => (
                     <div key={index}>
                       <h1>{item.username}</h1>
                       {item.profession.map((profession, profIndex) => (
-                          <div key={profIndex}>
-                            <h2>{profession.name}</h2>
-                            <p>Level: {profession.level}</p>
-                            <p>Salary: {profession.salary}</p>
-                            <ul>
-                              {profession.skills.map((skill, skillIndex) => (
-                                  <li key={skillIndex}>{skill}</li>
-                              ))}
-                            </ul>
-                            <h3>Courses:</h3>
-                            {profession.course.map((course, courseIndex) => (
-                              <div key={courseIndex}>
-                                <h4>{course.name}</h4>
-                                <p>Description: {course.description}</p>
-                                <p>Full Cost: {course.course_cost_full}</p>
-                                <p>Per Month: {course.course_per_month}</p>
-                                <a href={course.link_course} target="_blank" rel="noopener noreferrer">Link</a>
-                              </div>
+                        <div key={profIndex}>
+                          <h2>{profession.name}</h2>
+                          <p>Level: {profession.level}</p>
+                          <p>Salary: {profession.salary}</p>
+                          <ul>
+                            {profession.skills.map((skill, skillIndex) => (
+                              <li key={skillIndex}>{skill}</li>
                             ))}
-                          </div>
-                        ))}
-                      </div>
-                    ))}
-                  </div>
-                </Typography> */}
+                          </ul>
+                          <h3>Courses:</h3>
+                          {profession.course.map((course, courseIndex) => (
+                            <div key={courseIndex}>
+                              <h4>{course.name}</h4>
+                              <p>Description: {course.description}</p>
+                              <p>Full Cost: {course.course_cost_full}</p>
+                              <p>Per Month: {course.course_per_month}</p>
+                              <a href={course.link_course} target="_blank" rel="noopener noreferrer">Link</a>
+                            </div>
+                          ))}
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
+              </Typography>
             </Box>
           </Box>
         </Modal>
